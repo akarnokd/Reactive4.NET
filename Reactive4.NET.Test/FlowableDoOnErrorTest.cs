51e2a696fe125d126f8f85d0e7131e6813338741
Luke Horsley <luke.horsley@offset1337.co.uk>
2019-03-18 16:58:58 +0000

    FlowablePeek now invokes the onError delegate (#11)

---
+++ Reactive4.NET.Test/FlowableDoOnErrorTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Reactive4.NET.Test
+{
+    [TestFixture]
+    public class FlowableDoOnErrorTest
+    {
+        [Test]
+        public void Normal()
+        {
+            var seenErrors = new List<Exception>();
+
+            Flowable.Error<int>(new InvalidOperationException())
+                .DoOnError(seenErrors.Add)
+                .Test();
+
+            Assert.AreEqual(1, seenErrors.Count);
+        }
+        
+    }
+}
